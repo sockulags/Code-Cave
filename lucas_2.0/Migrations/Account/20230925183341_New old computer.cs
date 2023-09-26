ea4aeb2d164e736cc4313b475b3719a40569ee86
sockulags <125367352+sockulags@users.noreply.github.com>
2023-09-26 17:17:20 +0200

    styling

---
+++ lucas_2.0/Migrations/Account/20230925183341_New old computer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lucas_2._0.Migrations.Account
+{
+    /// <inheritdoc />
+    public partial class Newoldcomputer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
