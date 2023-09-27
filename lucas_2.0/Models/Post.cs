2f293970f9de8f094a3dbde0b47ce099b778d458
sockulags <125367352+sockulags@users.noreply.github.com>
2023-09-27 16:17:15 +0200

    Styling

--- lucas_2.0/Models/Post.cs
+++ lucas_2.0/Models/Post.cs
@@ -8,7 +8,8 @@ namespace lucas_2._0.Models
         [Key] public int Id { get; set; }
         [Required] public string Title { get; set; }
 
-        [Required] public string Description { get; set; }
+        [Required] 
+       public string Description { get; set; }
         public string Code { get; set; }
 
         public DateTime CreatedDate { get; set; } = DateTime.Now;
