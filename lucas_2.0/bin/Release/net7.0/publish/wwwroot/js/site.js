517bf2e38aa661776345328dfd3219c62d6f9e86
Lucas Skog <lucasskog@gmail.com>
2024-01-08 18:01:21 +0100

    probably a mistake

---
+++ lucas_2.0/bin/Release/net7.0/publish/wwwroot/js/site.js
@@ -0,0 +1,36 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+// selectors
+const themeToggleBtn = document.querySelector('.darkmode-input');
+
+// state
+const theme = localStorage.getItem('theme');
+
+// on mount
+theme && document.body.classList.add(theme);
+
+// handlers
+const handleThemeToggle = () => {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark-mode');
+    } else {
+        localStorage.removeItem('theme');
+    }
+    
+    var style = document.getElementById('style');
+    if (style.getAttribute('href') === '/css/prism.css') {
+        style.setAttribute('href', '/css/prism-darkmode.css');
+    } else {
+        style.setAttribute('href', '/css/prism.css');
+    }
+     
+
+};
+
+// events
+themeToggleBtn.addEventListener('click', handleThemeToggle);
+
+// change stylesheet using JavaScript
